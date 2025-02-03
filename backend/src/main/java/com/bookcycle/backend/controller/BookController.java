f5452ed3869742ec283ed82ef730292c5b579668
LaraMatosAguirres <lara.laramatos@gmail.com>
2025-02-03 20:28:40 -0300

    Crud complet BookController

--- backend/src/main/java/com/bookcycle/backend/controller/BookController.java
+++ backend/src/main/java/com/bookcycle/backend/controller/BookController.java
@@ -4,8 +4,7 @@ import com.bookcycle.backend.dto.BookDTO;
 import com.bookcycle.backend.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,29 @@ public class BookController {
     public ResponseEntity<List<BookDTO>> findAll(){
         List<BookDTO> list = bookService.findAllBooks();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<BookDTO> findById(@PathVariable Long id){
+        BookDTO dto = bookService.findBookById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<BookDTO> insertBook (BookDTO bookDTO){
+        BookDTO dto = bookService.createBook(bookDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping
+    public ResponseEntity<BookDTO> editBook(@PathVariable Long id, BookDTO bookDTO){
+        BookDTO dto = bookService.updateBook(id, bookDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<BookDTO> deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
     }
 }
